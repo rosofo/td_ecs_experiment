fd98fe9caa4a5b5f7a3131dd359f3e83e89aaefa
Robin Forbes <rsforbes0@gmail.com>
2025-01-11 10:14:26 +0000

    switch to bevy App

--- ecs/src/lib.rs
+++ ecs/src/lib.rs
@@ -1,19 +1,25 @@
 mod components;
 mod op;
-mod schedule;
 mod touchdesigner;
 mod world;
 
 use components::{OpComponent, Random};
 use pyo3::prelude::*;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
 use world::PyWorld;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn ecs(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    let appender = tracing_appender::rolling::never("logs", "bevy.log");
     tracing_subscriber::Registry::default()
         .with(tracing_tracy::TracyLayer::default())
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_writer(appender)
+                .with_filter(LevelFilter::INFO),
+        )
         .init();
 
     m.add_class::<PyWorld>()?;
--- ecs/src/world.rs
+++ ecs/src/world.rs
@@ -1,10 +1,10 @@
-use std::any::Any;
+use std::{any::Any, sync::Arc};
 
-use bevy_ecs::{
-    component::{ComponentInfo, Components},
+use bevy::{
+    app::ScheduleRunnerPlugin,
+    log::LogPlugin,
     prelude::*,
-    schedule::{InternedScheduleLabel, ScheduleLabel},
-    world::CommandQueue,
+    remote::{http::RemoteHttpPlugin, RemotePlugin},
 };
 use pyo3::prelude::*;
 use tracing::{debug, instrument};
@@ -12,28 +12,28 @@ use tracing::{debug, instrument};
 use crate::{
     components::{randomize_pars, OpComponent},
     op::Op,
-    schedule::{PostUpdate, Update},
     touchdesigner::{apply_deferred_td, TDApi, TDCommandQueue},
 };
 
 #[pyclass(name = "World")]
 pub struct PyWorld {
-    world: World,
-    update: InternedScheduleLabel,
-    post_update: InternedScheduleLabel,
+    app: App,
 }
+unsafe impl Sync for PyWorld {}
+unsafe impl Send for PyWorld {}
 
 impl PyWorld {
     #[instrument(skip(self))]
     fn id(&mut self, td_id: u32) -> Entity {
         debug!("Getting or creating entity for op {}", td_id);
-        self.world
+        let world = self.app.world_mut();
+        world
             .query::<(Entity, &Op)>()
-            .iter(&self.world)
+            .iter(world)
             .find_map(|(e, op)| if op.id == td_id { Some(e) } else { None })
             .unwrap_or_else(|| {
                 debug!("Spawning new entity for op: {}", td_id);
-                self.world.spawn(Op { id: td_id }).id()
+                world.spawn(Op { id: td_id }).id()
             })
     }
 }
@@ -44,23 +44,18 @@ impl PyWorld {
     #[instrument]
     fn new() -> Self {
         debug!("Creating new PyWorld instance");
-        let mut update = Schedule::new(Update);
-        let update_label = update.label();
-        update.add_systems(randomize_pars);
-        let mut post_update = Schedule::new(PostUpdate);
-        post_update.add_systems(report_world);
-        let post_update_label = post_update.label();
+        let mut app = App::new();
+        app.add_plugins(MinimalPlugins.set(ScheduleRunnerPlugin::run_once()));
+        app.add_plugins(RemotePlugin::default());
+        app.add_plugins(RemoteHttpPlugin::default());
+        app.add_systems(Update, randomize_pars);
+        // app.add_systems(PostUpdate, report_world);
+        app.insert_resource(TDCommandQueue { queue: Vec::new() });
 
-        let mut world = World::new();
-        world.insert_resource(TDCommandQueue { queue: Vec::new() });
+        app.finish();
+        app.cleanup();
 
-        world.add_schedule(update);
-        world.add_schedule(post_update);
-        Self {
-            world,
-            update: update_label,
-            post_update: post_update_label,
-        }
+        Self { app }
     }
 
     #[instrument(skip(self))]
@@ -70,10 +65,10 @@ impl PyWorld {
         match component {
             OpComponent::Random(comp) => {
                 debug!("Inserting Random component");
-                self.world.commands().entity(entity).insert(comp)
+                self.app.world_mut().commands().entity(entity).insert(comp)
             }
         };
-        self.world.flush();
+        self.app.world_mut().flush();
     }
 
     #[instrument(skip(self))]
@@ -83,29 +78,35 @@ impl PyWorld {
         match component {
             OpComponent::Random(comp) => {
                 debug!("Inserting Random component");
-                let id = self.world.components().get_id(comp.type_id()).unwrap();
-                self.world.commands().entity(entity).remove_by_id(id);
+                let id = self
+                    .app
+                    .world()
+                    .components()
+                    .get_id(comp.type_id())
+                    .unwrap();
+                self.app
+                    .world_mut()
+                    .commands()
+                    .entity(entity)
+                    .remove_by_id(id);
             }
         };
-        self.world.flush();
+        self.app.world_mut().flush();
     }
 
     #[instrument(skip(self))]
     fn despawn(&mut self, td_id: u32) {
         let entity = self.id(td_id);
-        self.world.commands().entity(entity).despawn();
-        self.world.flush();
+        self.app.world_mut().commands().entity(entity).despawn();
+        self.app.world_mut().flush();
     }
 
     #[instrument(skip(self))]
     fn run(&mut self) {
-        debug!("Running schedules");
-        self.world.run_schedule(self.update);
-        self.world.run_schedule(self.post_update);
-
+        self.app.update();
         Python::with_gil(|py| {
             let api = TDApi::new(py);
-            apply_deferred_td(&mut self.world, &api);
+            apply_deferred_td(self.app.world_mut(), &api);
         });
     }
 }
